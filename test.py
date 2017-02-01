598e9b9055ecf34c6ee7fcecc5ced89934183298
jolinchuh <linzhu@Jacks-MacBook-Pro-2.local>
2017-02-01 01:56:29 -0800

    smartcab project initial submission
    python -m smartcab.agent

---
+++ test.py
@@ -0,0 +1,7 @@
+import random
+action_option = ["left", "right", "forward", None] 
+a1 = random.choice(action_option)
+a2 = random.choice(action_option)
+a3 = random.choice(action_option)
+
+print a1, a2, a3
